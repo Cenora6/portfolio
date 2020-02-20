6a1e3e29288b68bd34578367aae1b179f7262eca
Cenora6 <aleksandra.gasidlo@gmail.com>
2020-02-20 19:15:00 +0100

    Added SCSS normalize | reset

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import {
+    HashRouter,
+    Route,
+    Switch
+} from 'react-router-dom';
+import Home from "./components/Home";
 
 function App() {
   return (
-    <div className="App">
-      <p>To jets portfolio</p>
-    </div>
+      <HashRouter>
+          <Switch>
+              <Route exact path='/' component={Home}/>
+          </Switch>
+      </HashRouter>
   );
 }
 
