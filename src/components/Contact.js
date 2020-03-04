4c52a75683a94de07a6bffbeabbe4d7428270d5c
Cenora6 <aleksandra.gasidlo@gmail.com>
2020-03-04 12:52:13 +0100

    Fixed contact navigation

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -12,7 +12,6 @@ class Contact extends Component {
     };
 
     render() {
-        console.log(this.state.hoverNavigation);
         return (
             <div className='contact'>
                 <div className={`contact_circles ${this.state.hoverNavigation ? "show_navigation" : "hide_navigation"}`}
@@ -21,27 +20,27 @@ class Contact extends Component {
                             <i className="far fa-address-book"></i>
                         </span>
                         <span className="contact_circles_single linkedin">
-                            <a href="https://www.linkedin.com/in/aleksandra-gasid%C5%82o-8b0338190/" className="menu-item">
+                            <a target="_blank" href="https://www.linkedin.com/in/aleksandra-gasid%C5%82o-8b0338190/">
                                 <i className="fab fa-linkedin-in"></i>
                             </a>
                         </span>
                         <span className="contact_circles_single facebook">
-                            <a href="https://www.facebook.com/Cenora06/" className="menu-item facebook">
+                            <a target="_blank" href="https://www.facebook.com/Cenora06/">
                                 <i className="fab fa-facebook"></i>
                             </a>
                         </span>
                         <span className="contact_circles_single github">
-                            <a href="https://github.com/Cenora6/" className=" menu-item github">
+                            <a target="_blank" href="https://github.com/Cenora6/">
                                 <i className="fab fa-github"></i>
                             </a>
                         </span>
                         <span className="contact_circles_single email">
-                            <a href="mailto:aleksandra.gasidlo@gmail.com" className=" menu-item email">
+                            <a target="_blank" href="mailto:aleksandra.gasidlo@gmail.com">
                                 <i className="fas fa-envelope"></i>
                             </a>
                         </span>
                         <span className="contact_circles_single instagram">
-                            <a href="https://www.instagram.com/wonthirst/" className=" menu-item instagram">
+                            <a target="_blank" href="https://www.instagram.com/wonthirst/">
                                 <i className="fab fa-instagram"></i>
                             </a>
                         </span>
